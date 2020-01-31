58487642f3b8d844aebcab6204bdbbbaf40219a1
Kokhungchan <43750758+Kokhungchan@users.noreply.github.com>
2020-01-31 14:36:27 +0800

    update navbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -23,9 +23,9 @@ const Header = () => {
   return (
     <body id="hero" className="jumbotron">
       <header>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
-          <a className="navbar-brand" href="#about">
-            Navbar
+        <nav className="navbar navbar-expand-lg navbar-dark">
+          <a className="navbar-brand">
+            KH
           </a>
           <button
             className="navbar-toggler"
@@ -40,15 +40,25 @@ const Header = () => {
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
+            <ul className="navbar-nav ml-lg-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#projects">
+                <a className="nav-link" href="#">
                   Home <span className="sr-only">(current)</span>
                 </a>
               </li>
               <li className="nav-item">
+                <a className="nav-link" href="#about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#projects">
+                  Project
+                </a>
+              </li>
+              <li className="nav-item">
                 <a className="nav-link" href="#contact">
-                  Link
+                  Contact
                 </a>
               </li>
             </ul>
